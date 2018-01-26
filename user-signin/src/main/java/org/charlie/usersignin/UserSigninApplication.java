d9ce9404c0eb3bdb0e0f181e68ae6938fbfe0a93
Charles Dill <31836603+cdi11@users.noreply.github.com>
2018-01-26 15:27:53 -0600

    Add files via upload

---
+++ user-signin/src/main/java/org/charlie/usersignin/UserSigninApplication.java
@@ -0,0 +1,12 @@
+package org.charlie.usersignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSigninApplication.class, args);
+	}
+}
---
+++ user-signin/src/test/java/org/charlie/usersignin/UserSigninApplicationTests.java
@@ -0,0 +1,16 @@
+package org.charlie.usersignin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserSigninApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
